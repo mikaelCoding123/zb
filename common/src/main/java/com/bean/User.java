acf441f6a51a6fcfa48caec315dec994fe8a6ab4
616164124@qq.com <616164124@qq.com>
2022-02-13 00:11:37 +0800

    init 2022年2月13日00:11:17

---
+++ common/src/main/java/com/bean/User.java
@@ -0,0 +1,33 @@
+package com.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String pokid;
+    private String username;
+    private String password;
+
+    public String getPokid() {
+        return pokid;
+    }
+
+    public void setPokid(String pokid) {
+        this.pokid = pokid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ common/src/main/java/com/common/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.common;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableDubbo
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
