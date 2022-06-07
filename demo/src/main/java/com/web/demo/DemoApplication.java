f65f80754f4c0e23006ca42003cd13209831fac9
616164124@qq.com <616164124@qq.com>
2022-06-07 18:37:28 +0800

    update 2022年6月7日18:15:03

---
+++ demo/src/main/java/com/web/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.web.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.web.*.dao",""})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+}
