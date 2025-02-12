82b59c01d20eac0f22ce4357368442751be08b47
aa <616164124@qq.com>
2025-02-13 02:58:16 +0800

    2025年2月13日aa

--- flinkTest/src/main/java/com/mikael/cn/day01/StreamWordCount.java
+++ flinkTest/src/main/java/com/mikael/cn/day01/StreamWordCount.java
@@ -10,7 +10,7 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(4);
-        String path = "C:\\2Code\\2BackendCode\\zb\\flinkTest\\src\\main\\java\\com\\mikael\\cn\\day01\\hello.txt";
+        String path = "Z:\\zb\\flinkTest\\src\\main\\java\\com\\mikael\\cn\\day01\\hello.txt";
         DataStream<String> stringDataStreamSource = env.readTextFile(path);
         DataStream<Tuple2<String, Integer>> sum = stringDataStreamSource.flatMap(new WordCount.MyflatMapper()).keyBy(0).sum(1).filter(x -> x.f1 > 2);
 
