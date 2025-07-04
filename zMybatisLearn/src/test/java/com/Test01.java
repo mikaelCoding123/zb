9e47d83971f4d41e7c63397de4dda8f93b9832a6
616164124@qq.com <616164124@qq.com>
2025-07-04 11:32:57 +0800

    11点21分qw

---
+++ zMybatisLearn/src/test/java/com/Test01.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author
+ * @version 1.0
+ * @date 2025/7/4
+ */
+
+@SpringBootTest
+public class Test01 {
+
+    @Test
+    void test01() {
+        System.out.println("1111");
+    }
+
+}
